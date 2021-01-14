70d5a10e3eb7889f47e9bd493aef0b61815ac1e9
iamngoni <imngonii@gmail.com>
2021-01-14 12:32:59 +0200

    test

---
+++ src/views/Home.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="container">
+    <div id="name-container">
+      <img id="avatar" src="./../assets/images/av.png" alt="avatar" />
+      <h1>Ngonidzashe Mangudya</h1>
+    </div>
+    <div class="title-desc-container">
+      <a id="github" href="https://github.com/iamngoni" target="_blank" rel="noopener noreferrer"
+        ><img class="title-font" src="./../assets/images/github.svg" />Developer</a
+      >
+      <span>/</span>
+      <a id="blog" href="https://twitter.com/iamngoni_" target="_blank" rel="noopener noreferrer"
+        ><img class="title-font" src="./../assets/images/twitter.svg" />Twimbo</a
+      >
+    </div>
+    <div class="article">
+      <h2>WHO AM I</h2>
+      <p>
+        I'm <i>Ngonidzashe Mangudya</i> a Software Engineering student by day and Developer by night. 
+      </p>
+
+      <h2>Academics</h2>
+      <ul>
+        <li>
+          🙇 Undergraduate student at HIT (🇿🇼
+          <a href="https://www.hit.ac.zw/" target="_blank" rel="noopener noreferrer">Harare Institute of Technology</a>), 08/2018
+          entry,
+          <i
+            ><a
+              href="https://www.hit.ac.zw/information-science-and-technology-software-engineering.html"
+              target="_blank"
+              rel="noopener noreferrer"
+              >B.Tech Hon. in SE</a
+            ></i
+          >.
+        </li>
+        
+        <li>🔭 <b>Fields of interest:</b> IoT</li>
+      </ul>
+
+      <h2>Devices</h2>
+      <ul>
+        <li>⚫ ThinkPad T550 5th Gen</li>
+        <li>🔵 iPhone SE</li>
+      </ul>
+
+      <h2>Contacts</h2>
+      <ul>
+        <li>
+          <b>Email：</b>
+          <a href="mailto:imngonii@gmail.com">imngonii@gmail.com</a><i> (Preferred)</i>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped>
+#name-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+#avatar {
+  width: 42px;
+  height: 42px;
+  border-radius: 50%;
+  margin-right: 12px;
+}
+
+#copyright:hover {
+  border: none;
+}
+
+.title-font {
+  width: 16px;
+  height: 16px;
+  margin-right: 4px;
+}
+
+.title-desc-container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin: 8px 0;
+}
+
+.title-desc-container a {
+  display: flex;
+  align-items: center;
+}
+
+.title-desc-container span {
+  margin: 0px 4px;
+}
+
+.title-desc-container #github {
+  color: #24292e;
+}
+
+.title-desc-container #sspai {
+  color: #ca2c2a;
+}
+
+.title-desc-container #blog {
+  color: #0070f3;
+}
+</style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="container">
+    <h1>Projects</h1>
+    <p>Projects I participated in and/or developed.</p>
+
+    <GitHubCard
+      title="📈 Covid 19 Stats Tracker (for Zimbabwe)"
+      link="https://github.com/flutterdevzim/CoVID-19-Tracker"
+      :info="c19tracker"
+      :loading="loading"
+    >
+      <p>
+        App to stay updated with the latest events regarding the Coronavirus.
+      </p>
+    </GitHubCard>
+
+    <GitHubCard title="📖 BulkSmsZw" link="https://github.com/iamngoni/bulksmszw" :info="bulksmszw" :loading="loading">
+      <p>
+        A Helper Library For Sending Text Messages For BulkSmsZimApi
+      </p>
+    </GitHubCard>
+
+    <GitHubCard
+      title="💡 Floating Navbar"
+      link="https://github.com/iamngoni/floating_navbar"
+      :info="floating_navbar"
+      :loading="loading"
+    >
+      <p>
+        Simple Flutter Floating Navbar package
+      </p>
+    </GitHubCard>
+
+    <GitHubCard
+      title="🇿🇼 Made-In-Zwe"
+      link="https://github.com/iamngoni/made-in-zwe"
+      :info="madeinzwe"
+      :loading="loading"
+    >
+      <p>A curation of awesome tools , apps and projects built by Zimbabwean developers</p>
+    </GitHubCard>
+
+    <GitHubCard
+      title="® localregex"
+      link="https://github.com/iamngoni/localregex"
+      :info="localregex"
+      :loading="loading"
+    >
+      <p>Dart implementation of Michael Dera's simple-regex typescript package</p>
+    </GitHubCard>
+
+    <GitHubCard
+      title="🍇 Fresh Idea"
+      link="https://github.com/iamngoni/freshidea"
+      :info="freshidea"
+      :loading="loading"
+    >
+      <p>Simple Flutter implementation of Fresh Ideas' Fresh Account Mobile Application</p>
+    </GitHubCard>
+
+    <GitHubCard
+      title="🏫 HitPrintShop"
+      link="https://hitprintshop.netlify.app/"
+      :info="hitprintshop"
+      :loading="loading"
+    >
+      <p>Online Assignment Submission And Printing for HIT students</p>
+    </GitHubCard>
+
+    <GitHubCard
+      title="🗺 Easy Locate"
+      link="https://github.com/iamngoni/Easy-Locate"
+      :info="easylocate"
+      :loading="loading"
+    >
+      <p>Product Locator Mobile Application in Flutter</p>
+    </GitHubCard>
+  </div>
+</template>
+
+<script>
+import GitHubCard from '@/components/GitHubCard.vue'
+
+export default {
+  components: {
+    GitHubCard,
+  },
+  data() {
+    return {
+      loading: true,
+      c19tracker: {
+        stargazers_count: 0,
+        forks_count: 0,
+      },
+      bulksmszw: {
+        stargazers_count: 0,
+        forks_count: 0,
+      },
+      floating_navbar: {
+        stargazers_count: 0,
+        forks_count: 0,
+      },
+      madeinzwe: {
+        stargazers_count: 0,
+        forks_count: 0,
+      },
+      localregex: {
+        stargazers_count: 0,
+        forks_count: 0,
+      },
+      freshidea: {
+        stargazers_count: 0,
+        forks_count: 0,
+      },
+      hitprintshop: {
+        stargazers_count: 0,
+        forks_count: 0,
+      },
+      easylocate: {
+        stargazers_count: 0,
+        forks_count: 0,
+      },
+    }
+  },
+  mounted() {
+    const githubApiUrl = 'https://api.github.com/repos'
+
+    const c19trackerAxios = this.axios.get(`${githubApiUrl}/flutterdevzim/CoVID-19-Tracker`)
+    const bulksmszwAxios = this.axios.get(`${githubApiUrl}/iamngoni/bulksmszw`)
+    const floatingAxios = this.axios.get(`${githubApiUrl}/iamngoni/floating_navbar`)
+    const madeInZweAxios = this.axios.get(`${githubApiUrl}/iamngoni/made-in-zwe`)
+    const localRegexAxios = this.axios.get(`${githubApiUrl}/iamngoni/localregex`)
+    const freshIdeaAxios = this.axios.get(`${githubApiUrl}/iamngoni/freshidea`)
+    const easyLocateAxios = this.axios.get(`${githubApiUrl}/iamngoni/Easy-Locate`)
+
+    this.axios
+      .all([c19trackerAxios, bulksmszwAxios, floatingAxios, madeInZweAxios, localRegexAxios, freshIdeaAxios, easyLocateAxios])
+      .then(
+        this.axios.spread((...resp) => {
+          this.loading = false
+          this.c19tracker = resp[0].data
+          this.bulksmszw = resp[1].data
+          this.floating_navbar = resp[2].data
+          this.madeinzwe = resp[3].data
+          this.localregex = resp[4].data
+          this.freshidea = resp[5].data
+          this.easylocate = resp[6].data
+        }),
+      )
+      .catch(err => {
+        this.loading = false
+        // eslint-disable-next-line no-console
+        console.error(err)
+      })
+  },
+}
+</script>
+
+<style scoped>
+.container .github-project-card:not(:last-child) {
+  margin-bottom: 40px;
+}
+</style>
